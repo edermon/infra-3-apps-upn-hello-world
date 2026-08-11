e635387ac1f0e1edbd006405cdd2f650dc4ac4f3
edermon <eder.mondragon@tektonlabs.com>
2026-08-11 22:42:51 +0000

    fix: bump public-exposure a v0.6.4 (cdn_policy requeria cache_key_policy)
    Error real: 'Missing required argument -- one of cdn_policy.0.cache_key_policy,
    cdn_policy.0.signed_url_cache_max_age_sec must be specified'. Corregido en
    infra-modules v0.6.4 (cache_key_policy explicito con los defaults reales de
    la API).

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ module "service" {
 }
 
 module "exposure" {
-  source = "git::https://github.com/edermon/infra-modules.git//modules/public-exposure?ref=v0.6.3"
+  source = "git::https://github.com/edermon/infra-modules.git//modules/public-exposure?ref=v0.6.4"
 
   app_name               = var.app_name
   project_id             = module.app_project.project_id
